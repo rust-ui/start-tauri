e7bbc54769961a15f05b9dc39b1ebe34716c1dbe
Max Wells <maxwells.pro@proton.me>
2026-03-05 09:13:44 +0700

    Remove unused files

--- to_remove/routing/mod.rs
+++
@@ -1,3 +0,0 @@
-pub mod app_route;
-pub mod home_page;
-pub mod not_found_page;
--- to_remove/routing/not_found_page.rs
+++
@@ -1,6 +0,0 @@
-use leptos::prelude::*;
-
-#[component]
-pub fn NotFoundPage() -> impl IntoView {
-    view! { "Not found" }
-}
